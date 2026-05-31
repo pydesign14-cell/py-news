c4c43604726e290a2966500815cad3281704087c
pydesign14-cell <pydesign14@gmail.com>
2026-05-31 09:12:30 +0700

    Add PY-NEWS JavaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-// PY-NEWS JavaScript
+/* MOBILE MENU */
+
+const menuBtn = document.querySelector(".mobile-menu-btn");
+
+const navbar = document.querySelector(".navbar");
+
+/* SAAT TOMBOL DIKLIK */
+
+menuBtn.addEventListener("click", () => {
+
+    navbar.classList.toggle("active");
+
+    // STICKY NAVBAR SCROLL EFFECT
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", function(){
+
+    if(window.scrollY > 50){
+        navbar.classList.add("scrolled");
+    }else{
+        navbar.classList.remove("scrolled");
+    }
+
+});
