14e83b40fab2c8a9c21aa8257a08f062c6fac48f
pydesign14-cell <pydesign14@gmail.com>
2026-06-01 21:11:58 +0700

    Refactor mobile menu and sticky navbar code

--- js/script.js
+++ js/script.js
@@ -1,17 +1,15 @@
 /* MOBILE MENU */
 
 const menuBtn = document.querySelector(".mobile-menu-btn");
-
 const navbar = document.querySelector(".navbar");
 
 /* SAAT TOMBOL DIKLIK */
 
 menuBtn.addEventListener("click", () => {
-
     navbar.classList.toggle("active");
+});
 
-    // STICKY NAVBAR SCROLL EFFECT
-const navbar = document.getElementById("navbar");
+/* STICKY NAVBAR SCROLL EFFECT */
 
 window.addEventListener("scroll", function(){
 
